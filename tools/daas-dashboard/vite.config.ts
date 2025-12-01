1fa0d2415bfbcc5aba738e5723347da444c56c1c
“JingleH” <jingleh321@gmail.com>
2025-12-01 11:29:06 -1000

    fixed build

--- tools/daas-dashboard/vite.config.ts
+++ tools/daas-dashboard/vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tools/daas-dashboard/dist/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     port: 3000,
   },
