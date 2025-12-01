a05fa3dff09d7a02507486bb80c6deeaeedb3a88
“JingleH” <jingleh321@gmail.com>
2025-12-01 13:01:44 -1000

    added tailwind

--- tools/daas-dashboard/vite.config.ts
+++ tools/daas-dashboard/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: command === 'serve' ? '/' : '/tools/daas-dashboard/dist/',
   build: {
     rollupOptions: {
