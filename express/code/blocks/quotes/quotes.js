2155809d23ba84b8eab550d28a86ae5655a0566b
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2025-12-05 10:58:53 -0600

    Squashed commit of the following:
    commit c95aaca1da09e33513f9c601c98cefde15264804
    Author: Eric Chen <159481679+echen-adobe@users.noreply.github.com>
    Date:   Fri Dec 5 10:37:31 2025 -0600
    
        Update quotes.js
    
    commit ef2ec70cb61e35610412e31bd402ed199bb2462a
    Merge: de89a62a e1d0bd29
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Dec 4 10:59:56 2025 -0800
    
        Merge pull request #808 from adobecom/test-gen-blocks
    
        Test gen blocks
    
    commit e1d0bd2957fa14d899c5b4df24301fe76510c9f0
    Merge: 66b62bf4 de89a62a
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Dec 4 09:22:28 2025 -0800
    
        Merge branch 'stage' into test-gen-blocks
    
    commit 66b62bf483bdd0e898022361ab0cdd3735ee02d8
    Merge: 4e6feae4 c9777803
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Dec 4 05:27:13 2025 -0800
    
        additional blocks and merge with stage
    
    commit 4e6feae4cc951a7ddd26b5618ca7b10ae7f34894
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Tue Dec 2 13:37:38 2025 -0800
    
        link-list-v2 and pricing-table
    
    commit 190f25c5dfa737879d3ea926bacf4081bf43c4a6
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Sun Nov 30 20:23:05 2025 -0800
    
        more blocks
    
    commit 3183867d19402a51051baaaa82af35eda22cc194
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Nov 20 08:46:42 2025 -0800
    
        new blocks:
    
    commit a4ad6105fd7200a250b528f583841339229f0e15
    Merge: aecbecc4 e80982c2
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Fri Nov 14 23:27:56 2025 -0800
    
        Merge branch 'stage' into test-gen-blocks
    
        merge from stage
    
    commit aecbecc4f496fa463d15702c6693a6230ee3158f
    Merge: 0a7fc966 b6e14f1f
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Nov 13 15:15:20 2025 -0800
    
        Merge branch 'stage' into test-gen-blocks
    
    commit 0a7fc9665d843e2fa5d624f1e230623aecdb6794
    Merge: cd17dd0e 5c1475dd
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Nov 13 14:27:03 2025 -0800
    
        Merge branch 'stage' into test-gen-blocks
    
    commit cd17dd0e23a2bb16b477dd29292466e531d8e3f0
    Author: Haris Bijli <all46558@adobe.com>
    Date:   Thu Nov 13 12:44:29 2025 -0800
    
        test-gen blocks

--- express/code/blocks/quotes/quotes.js
+++ express/code/blocks/quotes/quotes.js
@@ -404,9 +404,11 @@ export default async function decorate($block) {
 
       const $blockquote = createQuoteContent($card.firstElementChild.textContent, true);
       $card.firstElementChild.replaceWith($blockquote);
-      // Move author before content
-      if ($card.children.length > 1) {
-        $card.insertBefore($card.children[1], $card.firstElementChild);
+      // Move author before content without relying on child indexes
+      const author = $card.querySelector('.author');
+      const content = $card.querySelector('.content, blockquote');
+      if (author && content) {
+        $card.insertBefore(author, content);
       }
 
       $carouselContainer.appendChild($card);
