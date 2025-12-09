7c435a24c78c210018862680f22cd29f75a12aa5
Max Nelson <maxn@adobe.com>
2025-12-09 13:23:04 -0800

    refactored createCustomizationInputs to accommodate all atrribute types

--- express/code/blocks/print-product-detail/createComponents/customizationInputs/createCustomizationInputs.js
+++ express/code/blocks/print-product-detail/createComponents/customizationInputs/createCustomizationInputs.js
@@ -8,7 +8,10 @@ let createTag;
 
 async function createDynamicPillSelector(argumentObject) {
   const miniPillThreshold = 3;
-  if (argumentObject.productDetails.productType === 'zazzle_shirt' && argumentObject.attributeName === 'size') {
+  if (
+    argumentObject.productDetails.productType === 'zazzle_shirt'
+    && argumentObject.attributeName === 'size'
+  ) {
     return createStandardSelector(argumentObject);
   }
   if (argumentObject.attributeName === 'qty') {
@@ -24,12 +27,12 @@ export default async function createCustomizationInputs(
   productDetails,
   formDataObject = {},
 ) {
+  ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
   if (Object.keys(formDataObject).length === 0) {
     for (const [key, values] of Object.entries(productDetails.attributes)) {
       formDataObject[key] = values[0].name;
     }
   }
-  ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
   const customizationInputsContainer = createTag('div', {
     class: 'pdpx-customization-inputs-container',
     id: 'pdpx-customization-inputs-container',
@@ -38,18 +41,32 @@ export default async function createCustomizationInputs(
     id: 'pdpx-customization-inputs-form',
   });
   const selectorContainers = [];
+  const defaultAttributes = Object.keys(productDetails.attributes);
+  const qtyIndex = defaultAttributes.indexOf('qty');
+  if (qtyIndex !== -1) {
+    defaultAttributes.push(defaultAttributes.splice(qtyIndex, 1)[0]);
+  }
+
+  const attributesObject = {
+    zazzle_businesscard: ['style', 'cornerstyle', 'media', 'qty'],
+    zazzle_shirt: ['style', 'color', 'size', 'qty'],
+    default: defaultAttributes,
+  };
   const orderObject = {
     printquality: 'Print quality',
     cornerstyle: 'Corner style',
     qty: 'Quantity',
   };
-  let CTALinkText = null;
-  let drawerType = null;
+  const attributesObjectOrDefault = attributesObject[productDetails.productType]
+  || attributesObject.default;
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < Object.keys(productDetails.attributes).length; i++) {
-    const key = Object.keys(productDetails.attributes)[i];
+  for (let i = 0; i < attributesObjectOrDefault.length; i++) {
+    const key = attributesObjectOrDefault[i];
     const labelText = orderObject[key] || key.charAt(0).toUpperCase() + key.slice(1);
     const isTriBlend = formDataObject.style === 'triblend_shortsleeve3413';
+    const isBusinessCard = productDetails.productType === 'zazzle_businesscard';
+    let CTALinkText = null;
+    let drawerType = null;
     if (productDetails.productType === 'zazzle_shirt') {
       if (key === 'color') {
         CTALinkText = 'Learn More';
@@ -60,7 +77,10 @@ export default async function createCustomizationInputs(
         drawerType = 'sizeChart';
       }
     }
-    if (productDetails.productType === 'zazzle_businesscard' && key === 'media') {
+    if (
+      isBusinessCard
+      && key === 'media'
+    ) {
       CTALinkText = 'Compare Paper Types';
       drawerType = 'paperType';
     }
@@ -73,22 +93,10 @@ export default async function createCustomizationInputs(
       CTALinkText,
       drawerType,
     };
-    // eslint-disable-next-line no-continue
-    if (key === 'qty' || key === 'addon') continue;
-    const selectorContainer = await createDynamicPillSelector(argumentObject);
-    selectorContainers.push(selectorContainer);
-  }
-  if (productDetails.attributes.qty) {
-    const argumentObject = {
-      customizationOptions: productDetails.attributes.qty,
-      labelText: orderObject.qty,
-      attributeName: 'qty',
-      productDetails,
-      defaultValue: formDataObject.qty,
-      CTAText: null,
-    };
-    const quantitySelectorContainer = await createDynamicPillSelector(argumentObject);
-    selectorContainers.push(quantitySelectorContainer);
+    if (key !== 'addon') {
+      const selectorContainer = await createDynamicPillSelector(argumentObject);
+      selectorContainers.push(selectorContainer);
+    }
   }
   customizationInputsForm.append(...selectorContainers);
   customizationInputsContainer.appendChild(customizationInputsForm);
