75c23c93eef04977673cbce516bdee519c8bebc5
Max Nelson <maxn@adobe.com>
2026-02-04 12:03:13 -0800

    added console.log in order to push this branch after merge in attempt to solve stale code bus issue

--- express/code/blocks/print-product-detail/print-product-detail.js
+++ express/code/blocks/print-product-detail/print-product-detail.js
@@ -169,6 +169,7 @@ export default async function decorate(block) {
   await new Promise((resolve) => {
     loadStyle(`${config.codeRoot}/scripts/widgets/simple-carousel.css`, resolve);
   });
+  console.log(config.codeRoot);
   const { ietf } = config.locale;
   addPrefetchLinks(ietf);
   const templateId = extractTemplateId(block);
