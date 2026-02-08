3c69a6185079d4e742b519b9aad133bcfb67df2f
Yeiber Cano <yeibercano@gmail.com>
2026-02-07 23:24:28 -0800

    test: add wrong import to verify guard fails in CI (screenshot then revert)

---
+++ express/code/blocks/banner/guard-test-wrong-import.js
@@ -0,0 +1,6 @@
+/**
+ * TEMPORARY: Test that CI guard fails on illegal dev import.
+ * Remove after verifying PR fails. Do not use in production.
+ */
+import x from '../../dev-palettes/dev-palettes.js';
+export default x;
