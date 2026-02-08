3589af6b0a7ea5e746474ce96021639011d7034e
Yeiber Cano <yeibercano@gmail.com>
2026-02-07 23:22:23 -0800

    chore: remove guard test wrong-import file

--- express/code/blocks/banner/guard-test-wrong-import.js
+++
@@ -1,6 +0,0 @@
-/**
- * TEMPORARY: Test that CI guard fails on illegal dev import.
- * Remove after verifying PR fails. Do not use in production.
- */
-import x from '../../dev-palettes/dev-palettes.js';
-export default x;
