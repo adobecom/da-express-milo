341f7d1a892b24c14f3fdd2f39e86708d9d3930d
Brad Johnson <fullcolorcoder@gmail.com>
2026-02-11 10:17:07 -0600

    24 col grid

--- express/code/blocks/ax-grid-demo/ax-grid-demo.js
+++ express/code/blocks/ax-grid-demo/ax-grid-demo.js
@@ -6,22 +6,37 @@ export default async function init(block) {
   ({ createTag } = await import(`${getLibs()}/utils/utils.js`));
   // block.classList.add('ax-grid-container');
   block.innerHTML = '';
-  const fourThree = createTag('div', { class: 'ax-grid-container' });
-  fourThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '4*3'));
-  fourThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '4*3'));
-  fourThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '4*3'));
-  fourThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '4*3'));
+  const eightThree = createTag('div', { class: 'ax-grid-container' });
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
+  eightThree.append(createTag('div', { class: 'ax-grid-col-3 bar' }, '8*3'));
 
-  const fiveTwo = createTag('div', { class: 'ax-grid-container' });
-  fiveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '5*2'));
-  fiveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '5*2'));
-  fiveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '5*2'));
-  fiveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '5*2'));
-  fiveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '5*2'));
+  const twelveTwo = createTag('div', { class: 'ax-grid-container' });
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
+  twelveTwo.append(createTag('div', { class: 'ax-grid-col-2 bar' }, '12*2'));
 
-  const twoFour = createTag('div', { class: 'ax-grid-container' });
-  twoFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '2*4'));
-  twoFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '2*4'));
+  const sixFour = createTag('div', { class: 'ax-grid-container' });
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
+  sixFour.append(createTag('div', { class: 'ax-grid-col-4 bar' }, '6*4'));
 
   block.append(
     createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-1 bar' }, '1 col')),
@@ -36,8 +51,20 @@ export default async function init(block) {
     createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-10 bar' }, '10 col')),
     createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-11 bar' }, '11 col')),
     createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-12 bar' }, '12 col')),
-    fourThree,
-    fiveTwo,
-    twoFour,
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-13 bar' }, '13 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-14 bar' }, '14 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-15 bar' }, '15 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-16 bar' }, '16 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-17 bar' }, '17 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-18 bar' }, '18 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-19 bar' }, '19 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-20 bar' }, '20 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-21 bar' }, '21 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-22 bar' }, '22 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-23 bar' }, '23 col')),
+    createTag('div', { class: 'ax-grid-container' }, createTag('div', { class: 'ax-grid-col-24 bar' }, '24 col')),
+    eightThree,
+    twelveTwo,
+    sixFour,
   );
 }
