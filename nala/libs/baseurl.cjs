c8c225d9ae7dd5a85db56266024dcc222e1b64cb
Megan Thomas <methomas@adobe.com>
2026-02-12 09:17:45 -0800

    Revert "nala - branch test failing"
    This reverts commit b46b13e3a2180745072f6a4e8a707412b05c5df1.

--- nala/libs/baseurl.cjs
+++ nala/libs/baseurl.cjs
@@ -3,7 +3,7 @@ import { head } from 'axios';
 
 export async function isBranchURLValid(url) {
   try {
-    const response = await head(`${url}/express/`);
+    const response = await head(url);
     if (response.status === 200) {
       console.info(`\nURL (${url}) returned a 200 status code. It is valid.`);
       return true;
