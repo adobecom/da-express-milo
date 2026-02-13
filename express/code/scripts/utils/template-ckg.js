c857cdd90128a0db866459fce37a2d71e6949542
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-02-13 11:23:03 -0600

    Update template-ckg.js

--- express/code/scripts/utils/template-ckg.js
+++ express/code/scripts/utils/template-ckg.js
@@ -173,8 +173,16 @@ async function updateLinkList(container, linkPill, list) {
       searchLinks.push(clone);
     }
 
-    clone?.addEventListener('click', () => {
+    clone?.addEventListener('click', (e) => {
       const a = clone.querySelector(':scope > a');
+      const preventNavigate = getMetadata('search-marquee-pills-no-navigate');
+      const shouldPreventNavigate = preventNavigate && ['yes', 'true', 'on', 'Y'].includes(preventNavigate.toLowerCase());
+      
+      if (shouldPreventNavigate) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      
       updateImpressionCache({
         keyword_filter: d.displayValue,
         content_category: 'templates',
