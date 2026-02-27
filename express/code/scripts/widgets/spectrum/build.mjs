7ede5943b76970ae9d89c2e594ef0bd0a0d87fed
Dhananjay Singh Kanwar <kanwardhananjaysingh0545@gmail.com>
2026-02-28 02:34:25 +0530

    Add sp-icon-close to spectrum build and update icons-workflow.js
    - Included the `sp-icon-close` import in the spectrum build file to enhance icon availability.
    - Updated the `icons-workflow.js` file to reflect changes in the icon imports and maintain consistency in the icon library.

--- express/code/scripts/widgets/spectrum/build.mjs
+++ express/code/scripts/widgets/spectrum/build.mjs
@@ -189,6 +189,7 @@ const newComponents = [
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-chevron-down.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-chevron-left.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-add.js';",
+      "import '@spectrum-web-components/icons-workflow/icons/sp-icon-close.js';",
     ].join('\n'),
   },
 ];
