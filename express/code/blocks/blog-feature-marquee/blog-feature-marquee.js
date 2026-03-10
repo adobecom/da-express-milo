dfbde5bca2678ce16b90ae29c24da0f706b736c1
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-03-10 17:54:00 -0500

    new icon path

--- express/code/blocks/blog-feature-marquee/blog-feature-marquee.js
+++ express/code/blocks/blog-feature-marquee/blog-feature-marquee.js
@@ -6,7 +6,7 @@ let getMetadata;
 let getConfig;
 
 const MAX_ARTICLES = 6;
-const DEFAULT_PRODUCT_ICON_PATH = 'https://main--da-express-milo--adobecom.aem.page/express/learn/blog/assets/media_1f021705c13704e1e3041b414d0aa1ce883e067ec.png';
+const DEFAULT_PRODUCT_ICON_PATH = '/express/code/blocks/blog-feature-marquee/fallback_author_icon.png';
 const PRODUCT_ICON_SIZE = 48;
 
 const METADATA_KEYS = {
