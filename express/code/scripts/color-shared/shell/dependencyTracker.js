920d6ebab0b9182d893ed09b4552b614e7fab483
Yeiber Cano <yeibercano@gmail.com>
2026-03-12 14:45:19 -0700

    Adopt Milo loadStyle in shell/modal/toolbar loaders

--- express/code/scripts/color-shared/shell/dependencyTracker.js
+++ express/code/scripts/color-shared/shell/dependencyTracker.js
@@ -1,8 +1,25 @@
-import loadCSS from '../utils/loadCss.js';
+import { getLibs } from '../../utils.js';
 import { serviceManager } from '../../../libs/services/core/ServiceManager.js';
 
+let miloStyleLoaderPromise = null;
+
+async function loadMiloStyle(path) {
+  if (!miloStyleLoaderPromise) {
+    miloStyleLoaderPromise = import(`${getLibs()}/utils/utils.js`)
+      .then(({ loadStyle, getConfig }) => ({ loadStyle, getConfig }));
+  }
+
+  const { loadStyle, getConfig } = await miloStyleLoaderPromise;
+  const codeRoot = getConfig?.()?.codeRoot || '/express/code';
+  const href = path.startsWith('/') ? path : `${codeRoot}/${path}`;
+
+  return new Promise((resolve) => {
+    loadStyle(href, () => resolve());
+  });
+}
+
 export function createDependencyTracker(deps = {}) {
-  const cssLoader = deps.loadCSS || loadCSS;
+  const cssLoader = deps.loadCSS || loadMiloStyle;
   const serviceMgr = deps.serviceManager || serviceManager;
 
   const loadingServices = new Map();
