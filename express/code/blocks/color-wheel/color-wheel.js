2bb1ee894966dbbc86ac5621314b6fed82bdc425
Megan Thomas <methomas@adobe.com>
2026-03-13 15:20:35 -0700

    layout sidebar

--- express/code/blocks/color-wheel/color-wheel.js
+++ express/code/blocks/color-wheel/color-wheel.js
@@ -4,8 +4,9 @@ export default async function decorate(block) {
   block.innerHTML = '';
   block.className = 'color-wheel';
 
-  const col1 = createTag('div', { class: 'col1' });
-  const col2 = createTag('div', { class: 'col2' });
+  const sidebar = createTag('div', { class: 'color-palette-sidebar' });
 
-  block.append(col1, col2);
+  // Tabs
+
+  block.append(sidebar);
 }
