9506818d95d4cb362da6bce68afc625005173a84
Yeiber Cano <yeibercano@gmail.com>
2026-03-24 03:34:01 -0700

    fix(spectrum-icons): bundle sp-icon-drag-handle for swatch rail

--- express/code/scripts/widgets/spectrum/build.mjs
+++ express/code/scripts/widgets/spectrum/build.mjs
@@ -205,6 +205,7 @@ const newComponents = [
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-circle.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-copy.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-delete.js';",
+      "import '@spectrum-web-components/icons-workflow/icons/sp-icon-drag-handle.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-edit.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-share-android.js';",
       "import '@spectrum-web-components/icons-workflow/icons/sp-icon-target.js';",
--- express/code/scripts/widgets/spectrum/src/icons-workflow.js
+++ express/code/scripts/widgets/spectrum/src/icons-workflow.js
@@ -4,6 +4,7 @@ import '@spectrum-web-components/icons-workflow/icons/sp-icon-alert-triangle.js'
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-circle.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-copy.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-delete.js';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-drag-handle.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-edit.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-share-android.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-target.js';
