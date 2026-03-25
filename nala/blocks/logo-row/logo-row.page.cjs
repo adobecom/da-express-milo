e22d3ce7f33636ef881cd492b12bad4a1f57c38e
Max Nelson <maxn@adobe.com>
2026-03-25 15:30:03 -0700

    test change in order to observe nala test behavior

--- nala/blocks/logo-row/logo-row.page.cjs
+++ nala/blocks/logo-row/logo-row.page.cjs
@@ -2,7 +2,7 @@ export default class LogoRow {
   constructor(page, nth = 0) {
     this.page = page;
     this.nth = nth;
-    // section and logo-row wrapper
+    // section and logo-row wrapper.
     this.section = page.locator('.section').nth(nth);
     this.logoRow = page.locator('.logo-row').nth(nth);
     this.logoRowHeading = this.logoRow.locator('h5');
