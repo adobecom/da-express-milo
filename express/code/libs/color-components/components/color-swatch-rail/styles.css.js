493ddb442284c5058545fb9c8d727c1b5658efb9
Megan Thomas <methomas@adobe.com>
2026-03-25 19:22:06 -0700

    add todo note

--- express/code/libs/color-components/components/color-swatch-rail/styles.css.js
+++ express/code/libs/color-components/components/color-swatch-rail/styles.css.js
@@ -24,6 +24,7 @@ export const style = css`
     overflow: hidden;
   }
   
+  // TODO: This causes a problem on desktop with 5 colors (adds a scroll)
   .swatch-rail:has(.add-slot--column-left),
   .swatch-rail:has(.add-slot--column-right),
   .swatch-rail[data-orientation="vertical"].vertical--two-rows:has(.add-slot--column-left),
