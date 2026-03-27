500ec1dca9dfbc334d71a98277bf7c9472cbb2ee
Yeiber Cano <yeibercano@gmail.com>
2026-03-27 14:41:13 -0700

    [Color][MWPW-188926] Codebus lit deps entry module

--- express/code/libs/deps/lit.js
+++ express/code/libs/deps/lit.js
@@ -1,2 +1,7 @@
 export * from './lit-all.min.js';
-export { html, css, LitElement, classMap } from './lit-all.min.js';
+export {
+  html,
+  css,
+  LitElement,
+  classMap,
+} from './lit-all.min.js';
