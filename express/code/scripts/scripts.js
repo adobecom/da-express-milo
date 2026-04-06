f7f6d29f37453ec15c927432e1b1060aef54da72
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-04-06 11:06:32 -0500

    cn

--- express/code/scripts/scripts.js
+++ express/code/scripts/scripts.js
@@ -407,8 +407,8 @@ async function loadPage() {
      and not a 404 page (which would create an infinite redirect loop) */
   
   const isAdobeOrigin = /^(www\.stage\.|www\.)adobe\.com$/.test(window.location.hostname);
-  import('./utils/location-utils.js').then(({ getCountry }) => getCountry()).then((country) => {
-  //  if (country === 'cn' && isAdobeOrigin) { window.location.href = '/cn'; }
+    import('./utils/location-utils.js').then(({ getCountry }) => getCountry()).then((country) => {
+      if (country === 'cn' && isAdobeOrigin) { window.location.href = '/cn'; }
   });
 
 
