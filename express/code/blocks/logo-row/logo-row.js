bdea28fe791b755d9cc741931669b9ccf66474d5
Max Nelson <maxn@adobe.com>
2026-04-09 00:37:20 -0700

    adjusted logo-row block to use new figma_typography.css file

--- express/code/blocks/logo-row/logo-row.js
+++ express/code/blocks/logo-row/logo-row.js
@@ -1,20 +1,26 @@
-export default function decorate(block) {
-  // Add class to the main block
+import { getLibs } from '../../scripts/utils.js';
+
+export default async function decorate(block) {
+  const { loadStyle, getConfig } = await import(`${getLibs()}/utils/utils.js`);
+  const { codeRoot } = getConfig();
+  loadStyle(`${codeRoot}/styles/figma/figma_typography.css`);
+
   block.classList.add('block-layout');
+  block.classList.add('figma-system');
 
   const row = block.children[0];
   row.classList.add('block-row');
 
   const [textColumn, imageColumn] = row.children;
 
-  // Style text column
   textColumn.classList.add('text-column');
 
-  // Style image column
-  imageColumn.classList.add('image-column');
+  const title = textColumn.children[0];
+  title.classList.add('figma-heading-s');
 
+  imageColumn.classList.add('image-column');
   const images = imageColumn.querySelectorAll('img');
-  // Add classes to all images
+
   images.forEach((img) => img.classList.add('brand-image'));
   if (images.length > 5) block.classList.add('numerous');
 }
