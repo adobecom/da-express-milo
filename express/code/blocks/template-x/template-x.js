fc0830411199959a245a41a53860209994c0b4e1
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-04-15 10:09:18 -0500

    reworked the query overrides

--- express/code/blocks/template-x/template-x.js
+++ express/code/blocks/template-x/template-x.js
@@ -22,6 +22,7 @@ import {
 import { fetchResults, isValidTemplate as isValidTemplateTaas } from '../../scripts/template-utils.js';
 import fetchAllTemplatesMetadata from '../../scripts/utils/all-templates-metadata.js';
 import renderTemplate from './template-rendering.js';
+import { applyTaasToolbarOverrides } from './taas-query-overrides.js';
 import isDarkOverlayReadable from '../../scripts/color-tools.js';
 import BlockMediator from '../../scripts/block-mediator.min.js';
 import buildGallery from '../../scripts/widgets/gallery/gallery.js';
@@ -112,32 +113,7 @@ async function fetchAndRenderTemplates(props, options = {}) {
 async function fetchAndRenderTemplatesFromTaas(taasQuery, props, options = {}) {
   const { isFirstLoad = false } = options;
   const queryParams = new URLSearchParams(taasQuery);
-  if (props?.start) queryParams.set('start', props.start);
-
-  // Apply toolbar filter/sort overrides so user selections affect TAAS results
-  if (props?.filters?.premium && props.filters.premium !== 'all') {
-    queryParams.set('license', props.filters.premium.toLowerCase() === 'false' ? 'free' : 'premium');
-  } else if (props?.filters?.premium === 'all') {
-    queryParams.delete('license');
-  }
-  if (props?.filters?.animated && props.filters.animated !== 'all') {
-    queryParams.set('behaviors', props.filters.animated.toLowerCase() === 'false' ? 'still' : 'animated');
-  } else if (props?.filters?.animated === 'all') {
-    queryParams.delete('behaviors');
-  }
-  if (props?.sort) {
-    const sortToOrderBy = {
-      'Most Viewed': '-remixCount',
-      'Rare & Original': 'remixCount',
-      'Newest to Oldest': '-createDate',
-      'Oldest to Newest': 'createDate',
-    };
-    if (sortToOrderBy[props.sort]) {
-      queryParams.set('orderBy', sortToOrderBy[props.sort]);
-    } else {
-      queryParams.delete('orderBy');
-    }
-  }
+  applyTaasToolbarOverrides(queryParams, props);
 
   const res = await fetchResults(queryParams.toString());
 
