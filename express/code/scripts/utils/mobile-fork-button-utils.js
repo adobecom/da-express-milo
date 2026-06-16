e740ef2a7e56c05d7931758be72bbd31d00d0d38
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-06-16 17:08:12 -0500

    Update mobile-fork-button-utils.js

--- express/code/scripts/utils/mobile-fork-button-utils.js
+++ express/code/scripts/utils/mobile-fork-button-utils.js
@@ -4,7 +4,7 @@
  */
 
 export const LONG_TEXT_CUTOFF = 70;
-
+export const SUPPORTED_MWEB_OS = ['Android', 'iOS'];
 /**
  * Calculates the pixel width of text for a given font
  * @param {string} text - The text to measure
