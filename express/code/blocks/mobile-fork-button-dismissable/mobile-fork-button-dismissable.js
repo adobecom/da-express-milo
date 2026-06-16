ef28c5ce1feefec9b687433bbddc2e66d96385e1
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-06-16 17:16:43 -0500

    init

--- express/code/blocks/mobile-fork-button-dismissable/mobile-fork-button-dismissable.js
+++ express/code/blocks/mobile-fork-button-dismissable/mobile-fork-button-dismissable.js
@@ -1,6 +1,6 @@
 import { getLibs, getMobileOperatingSystem, getIconElementDeprecated, addTempWrapperDeprecated } from '../../scripts/utils.js';
 import { createFloatingButton } from '../../scripts/widgets/floating-cta.js';
-import { createMultiFunctionButton, androidCheck, collectFloatingButtonData, createMetadataMap } from '../../scripts/utils/mobile-fork-button-utils.js';
+import { createMultiFunctionButton, collectFloatingButtonData, createMetadataMap, SUPPORTED_MWEB_OS } from '../../scripts/utils/mobile-fork-button-utils.js';
 
 let createTag; let getMetadata;
 
