540bd689ad8d9dbd80581ea3b7aa5977d2a8cc0c
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-07-06 14:39:21 -0500

    tests

---
+++ nala/blocks/icon-carousel/icon-carousel.spec.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'Express icon-carousel block',
+  features: [
+    {
+      tcid: '0',
+      name: '@icon-carousel default',
+      path: '/drafts/echen/icon-carousel',
+      selector: '.icon-carousel',
+      data: {},
+      tags: '@icon-carousel @express @smoke @regression @t1',
+    },
+    {
+      tcid: '1',
+      name: '@icon-carousel dark variant',
+      path: '/drafts/echen/icon-carousel',
+      selector: '.icon-carousel.dark',
+      data: {},
+      tags: '@icon-carousel @express @regression @t2',
+    },
+  ],
+};
