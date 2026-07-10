315c8d23c0af16cdafbed0aa0762b64b0d18022a
Eric Chen <159481679+echen-adobe@users.noreply.github.com>
2026-07-10 15:32:31 -0500

    nala

--- nala/blocks/font-generator/font-generator.spec.cjs
+++ nala/blocks/font-generator/font-generator.spec.cjs
@@ -1,3 +1,17 @@
-const schema = require('./font-generator.block.json');
-
-module.exports = { features: schema.variants };
+module.exports = {
+  name: 'Express font-generator block',
+  features: [
+    {
+      tcid: '0',
+      name: '@font-generator default',
+      path: '/drafts/echen/font-generator',
+      selector: '.font-generator',
+      data: {
+        defaultPreviewText: 'The quick brown fox jumps over the lazy dog',
+        filterTriggerLabel: 'Filter',
+        initialLayout: 'list',
+      },
+      tags: '@font-generator @express @smoke @regression @t1',
+    },
+  ],
+};
