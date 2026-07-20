9dd4fba9f39f128d0adddfce9763c4f1d7a7135b
ShubhangiMG <shubhangig@adobe.com>
2026-07-20 11:06:15 +0530

    lint fix

--- express/code/blocks/susi-light/susi-light.js
+++ express/code/blocks/susi-light/susi-light.js
@@ -518,7 +518,7 @@ function blurModalCurtain() {
 
 export default async function init(el) {
   ({ createTag, loadScript, getConfig, loadIms } = await import(`${getLibs()}/utils/utils.js`));
-  if (el.classList.contains('b2b')) el.classList.add('context-business'); //temp for test
+  if (el.classList.contains('b2b')) el.classList.add('context-business'); // TEMP preview only — remove before merge
   isStage = (usp.get('env') && usp.get('env') !== 'prod') || getConfig().env.name !== 'prod';
   const locale = getConfig().locale.ietf.toLowerCase();
   const { imsClientId } = getConfig();
