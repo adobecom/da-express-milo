b46a903af8f72ecd4a1dbc83fa2d0dcb303922af
shikshachauhan <chauhanshiksha1@gmail.com>
2026-09-02 14:36:26 +0530

    Fix lint and test cases

--- test/scripts/utils/mini-editor-analytics.test.js
+++ test/scripts/utils/mini-editor-analytics.test.js
@@ -7,10 +7,12 @@ describe('mini-editor analytics', () => {
 
   beforeEach(() => {
     trackStub = sinon.stub();
+    // eslint-disable-next-line no-underscore-dangle
     window._satellite = { track: trackStub };
   });
 
   afterEach(() => {
+    // eslint-disable-next-line no-underscore-dangle
     delete window._satellite;
     sinon.restore();
   });
@@ -24,16 +26,19 @@ describe('mini-editor analytics', () => {
     expect(trackStub.calledOnce).to.be.true;
     const [eventName, payload] = trackStub.firstCall.args;
     expect(eventName).to.equal('event');
+    // eslint-disable-next-line no-underscore-dangle
     expect(payload.data._adobe_corpnew.sdm.event).to.deep.equal({
       pagename: 'export-project-complete-unauth',
       url: window.location.href,
     });
+    // eslint-disable-next-line no-underscore-dangle
     expect(payload.data._adobe_corpnew.sdm.custom).to.deep.equal({
       export_method: 'copy-clipboard',
       ui: {
         location: 'seo-discover-page-collapsible-row',
       },
     });
+    // eslint-disable-next-line no-underscore-dangle
     expect(payload.data._adobe_corpnew.custom).to.deep.equal([
       {
         propertyName: 'event.pagename',
@@ -43,6 +48,31 @@ describe('mini-editor analytics', () => {
       {
         propertyName: 'event.url',
         propertyValue: window.location.href,
+        propertyType: 'string',
+      },
+      {
+        propertyName: 'event.workflow',
+        propertyValue: 'export',
+        propertyType: 'string',
+      },
+      {
+        propertyName: 'event.type',
+        propertyValue: 'success',
+        propertyType: 'string',
+      },
+      {
+        propertyName: 'event.subtype',
+        propertyValue: 'export-project',
+        propertyType: 'string',
+      },
+      {
+        propertyName: 'event.category',
+        propertyValue: 'WEB',
+        propertyType: 'string',
+      },
+      {
+        propertyName: 'event.subcategory',
+        propertyValue: 'document',
         propertyType: 'string',
       },
       {
